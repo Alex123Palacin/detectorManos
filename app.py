d2e9601117905bc377f775e5bb9804179c045514
Alex123Palacin <apalacinal@ucvvirtual.edu.pe>
2025-06-01 23:24:06 -0500

    l

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ def video_feed():
                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True)
+    app.run( threaded=True)
